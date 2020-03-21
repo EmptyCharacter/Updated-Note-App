f95edf0ecd862e92de0428e6bb10cab5eefedb24
Joshua Farin <farin.k.joshua@gmail.com>
2020-03-21 03:57:07 -1000

    It is working... !

--- NoteApp/MainWindow.xaml.cs
+++ NoteApp/MainWindow.xaml.cs
@@ -42,10 +42,11 @@ namespace NoteApp
 
     public partial class MainWindow : Window
     {
-        private HashSet<NoteObject> NoteSet;
+        private HashSet<NoteObject> NoteSet = new HashSet<NoteObject>();
         private DateTime timeSinceAutoSave;
-        
-        
+        Timer autoSaveTimer = new Timer();
+
+
 
         public MainWindow()
         {
@@ -94,7 +95,7 @@ namespace NoteApp
 
         private void SaveFile(string stringToSave)
         {
-
+            
             NoteObject thisNote = new NoteObject(NotePad, stringToSave);
             bool isEmpty = (NoteSet.Count == 0);
             
@@ -134,16 +135,16 @@ namespace NoteApp
         private string GetRichTextBoxContent(RichTextBox richTextBox)
         {
             
-            RichTextBox myRichTextBox = new RichTextBox();
-            myRichTextBox = NotePad;
+            //RichTextBox myRichTextBox = new RichTextBox();
+            
 
-            FlowDocument myFlowDoc = new FlowDocument();
+           // FlowDocument myFlowDoc = new FlowDocument();
 
-            myRichTextBox.Document = myFlowDoc;
+            //myRichTextBox.Document = myFlowDoc;
 
             TextRange textRange = new TextRange(
-                    myRichTextBox.Document.ContentStart,
-                    myRichTextBox.Document.ContentEnd
+                    richTextBox.Document.ContentStart,
+                    richTextBox.Document.ContentEnd
                 );
             
             return textRange.Text;
@@ -151,16 +152,22 @@ namespace NoteApp
         private void OnAutoSaveTimer(object sender, ElapsedEventArgs e)
         {
             double autoSaveInterval = 2;
+            
             double test = ((e.SignalTime - timeSinceAutoSave).TotalSeconds);
             
             
             if (test > autoSaveInterval)
             {
+                
                 //gets the text from richtextbox
-                SaveFile(GetRichTextBoxContent(NotePad));
+
                 //set time since autosave to the date time now
                 Console.WriteLine("it works!");
+                
+                SaveFile(GetRichTextBoxContent(NotePad));
+                autoSaveTimer.Enabled = false;
                 timeSinceAutoSave = DateTime.Now;
+
             }
         }
 
@@ -171,6 +178,7 @@ namespace NoteApp
             timeSinceAutoSave = DateTime.Now;
             Timer autoSaveTimer = new Timer(2000);
             autoSaveTimer.Elapsed += OnAutoSaveTimer;
+            autoSaveTimer.AutoReset = false;
             autoSaveTimer.Enabled = true;
 
         }
